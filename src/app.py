b93c530503104dc356f909d9b75bad56023d5a51
Alain <alain.sanchez.gonzalez@gmail.com>
2025-03-27 12:17:54 +0100

    Aniadido formato black

--- src/app.py
+++ src/app.py
@@ -3,78 +3,75 @@ from flask import request, redirect, url_for, session
 
 
 app = Flask(__name__)
-app.secret_key = 'clave_secreta'
+app.secret_key = "clave_secreta"
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
 # Ruta simple
-@app.route('/api/data')
+@app.route("/api/data")
 def get_data():
-    return jsonify({
-        'menasje': 'Hola desde API Flask',
-        'estado': 'exitoso'
-    })
+    return jsonify({"menasje": "Hola desde API Flask", "estado": "exitoso"})
 
 
 # Ruta con parámetros
-@app.route('/saludo/<nombre>')
+@app.route("/saludo/<nombre>")
 def saludo(nombre):
-    return f'Hola, {nombre}!. Bienvenido a Flask'
+    return f"Hola, {nombre}!. Bienvenido a Flask"
 
 
 # Ruta con método POST
-@app.route('/enviar', methods=['POST'])
+@app.route("/enviar", methods=["POST"])
 def recibir_datos():
-    datos = request.form['dato']
-    return f'Dato recibido: {datos}'
+    datos = request.form["dato"]
+    return f"Dato recibido: {datos}"
 
 
 # Manejo de sesiones
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        usuario = request.form['usuario']
-        session['usuario'] = usuario
-        return redirect(url_for('perfil'))
-    return render_template('login.html')
+    if request.method == "POST":
+        usuario = request.form["usuario"]
+        session["usuario"] = usuario
+        return redirect(url_for("perfil"))
+    return render_template("login.html")
 
 
-@app.route('/perfil')
+@app.route("/perfil")
 def perfil():
-    if 'usuario' in session:
-        return f'Perfil de {session['usuario']}'
-    return redirect(url_for('home'))
+    if "usuario" in session:
+        return f"Perfil de {session['usuario']}"
+    return redirect(url_for("home"))
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
-    session.pop('usuario', None)
-    return redirect(url_for('home'))
+    session.pop("usuario", None)
+    return redirect(url_for("home"))
 
 
-@app.route('/suma/<int:a>/<int:b>')
+@app.route("/suma/<int:a>/<int:b>")
 def suma(a, b):
     # curl -X GET http://127.0.0.1:5000/suma/10/2
-    return jsonify({'Resultado': a+b}), 200
+    return jsonify({"Resultado": a + b}), 200
 
 
-@app.route('/resta/<int:a>/<int:b>')
+@app.route("/resta/<int:a>/<int:b>")
 def resta(a, b):
     # curl -X GET http://127.0.0.1:5000/resta/10/2
-    return jsonify({'Resultado': a-b}), 200
+    return jsonify({"Resultado": a - b}), 200
 
 
-@app.route('/multiplicacion/<int:a>/<int:b>')
+@app.route("/multiplicacion/<int:a>/<int:b>")
 def nultiplicacion(a, b):
     # curl -X GET http://127.0.0.1:5000/multiplicacion/10/2
-    return jsonify({'Resultado': a*b}), 200
+    return jsonify({"Resultado": a * b}), 200
 
 
-@app.route('/division/<int:a>/<int:b>')
+@app.route("/division/<int:a>/<int:b>")
 def division(a, b):
     # curl -X GET http://127.0.0.1:5000/division/10/2
     if b == 0:
@@ -82,5 +79,5 @@ def division(a, b):
     return jsonify({"Resultado": float(a / b)})
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- tests/test_api.py
+++ tests/test_api.py
@@ -12,28 +12,28 @@ def client():
 
 # Prueba suma
 def test_suma(client):
-    respuesta = client.get('/suma/5/3')
+    respuesta = client.get("/suma/5/3")
     assert respuesta.status_code == 200
-    assert respuesta.get_json()['Resultado'] == 8
+    assert respuesta.get_json()["Resultado"] == 8
 
 
 # Prueba resta
 def test_resta(client):
-    respuesta = client.get('/resta/5/3')
+    respuesta = client.get("/resta/5/3")
     assert respuesta.status_code == 200
-    assert respuesta.get_json()['Resultado'] == 2
+    assert respuesta.get_json()["Resultado"] == 2
 
 
 # Prueba multiplicacion
 def test_multiplicacion(client):
-    respuesta = client.get('/multiplicacion/5/3')
+    respuesta = client.get("/multiplicacion/5/3")
     assert respuesta.status_code == 200
-    assert respuesta.get_json()['Resultado'] == 15
+    assert respuesta.get_json()["Resultado"] == 15
 
 
 # Prueba division
 def test_division(client):
-    with patch('src.app.division', return_value={'Resultado': 2.5}):
-        respuesta = client.get('division/5/2')
+    with patch("src.app.division", return_value={"Resultado": 2.5}):
+        respuesta = client.get("division/5/2")
         assert respuesta.status_code == 200
-        assert respuesta.get_json()['Resultado'] == 2.5
+        assert respuesta.get_json()["Resultado"] == 2.5
